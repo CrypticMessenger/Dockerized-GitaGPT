2ed992ab85ad0089a7ee2ce94d29bd4937faa2d0
Ankit <2020csb1072@iitrpr.ac.in>
2023-09-30 21:52:43 +0530

    first commit

---
+++ backend.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, request
+from transformers import AutoTokenizer
+import transformers
+import torch
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['HF_ACCESS_TOKEN'] = os.getenv('HF_ACCESS_TOKEN')
+app.config['BACKEND_PORT'] = os.getenv('BACKEND_PORT')
+
+
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model, token = app.config['HF_ACCESS_TOKEN'])
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+    token=app.config['HF_ACCESS_TOKEN']
+)
+
+@app.route('/test_args', methods=['GET'])
+def test_args():
+    param2 = request.args.get('top_p')
+    param3 = request.args.get('prompt')
+
+    # Use the parameters in your API logic
+    result = f"Received parameters: top_p={param2}"
+    return param3 + result
+
+@app.route("/test")
+def test():
+    return "Hello, World!"
+
+@app.route("/enlighten", methods=['GET'])
+def enlighten():
+    num_return_sequences = request.args.get('num_return_sequences')
+    top_k = request.args.get('top_k')
+    max_length = request.args.get('max_length')
+    prompt = request.args.get('prompt')
+
+    sequences = pipeline(
+        prompt,
+        do_sample=True,
+        top_k=top_k,
+        num_return_sequences=num_return_sequences,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=max_length,
+    )
+    return sequences[0]["generated_text"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=app.config['BACKEND_PORT'])
---
+++ frontend.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="Gita GPT",page_icon=":peacock:")
+assistant_avatar = "✨"
+user_avatar = "🙏"
+
+# Inference URL
+inference_url = "http://127.0.0.1:5000/enlighten"
+test_url = "http://127.0.0.1:5000/test"
+test_args_url = "http://127.0.0.1:5000/test_args"
+
+
+
+st.title("Gita's Streamlit App 🦚🕉️")
+
+with st.sidebar:
+    st.title('🦙💬 Gita Chatbot')
+    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Hare Krishna! I am Gita, a chatbot trained on the Bhagavad Gita. How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    if(message["role"] == "user"):
+        with st.chat_message(message["role"],avatar=user_avatar):
+            st.write(message["content"])
+    else:
+        with st.chat_message(message["role"],avatar=assistant_avatar):
+            st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "Hare Krishna! I am Gita, a chatbot trained on the Bhagavad Gita. How may I assist you today?"}]
+
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating GitaGPT response
+def generate_gita_response(prompt):
+
+    # Starting prompt
+    string_dialogue = "you're here to selflessly help and answer any question or dilemma of anyone who comes to you. Analyze the person's question below and identify the base emotion and the root for this emotion, and then frame your answer by summarizing how the verses below apply to their situation and be emphatetic in your answer.  You are a helpful assistant. [context]:\n\n"
+    
+    for dict_message in st.session_state.messages[-6:]:
+        if(dict_message["role"] == "user"):
+            string_dialogue+= dict_message["content"] + "\n"
+
+    string_dialogue += "You give random bhagwat gita quotes to uplift user, now answer:\n"
+
+    final_prompt = f"{string_dialogue} {prompt}"
+    params ={"top_p":top_p, "max_length":max_length, "repetition_penalty":1, "prompt": final_prompt }
+    sz = len(final_prompt)
+
+    # print(requests.get(test_args_url, params=params).text)
+
+    # response = requests.get(test_args_url, params=params).text[sz:]
+    response = requests.get(inference_url, params=params).text[sz:]
+
+    return response
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user",avatar=user_avatar):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant",avatar=assistant_avatar):
+        with st.spinner("Thinking..."):
+            response = generate_gita_response(prompt)
+            placeholder = st.empty()
+           
+            
+            placeholder.markdown(response)
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
